042741a7d67edbb18b2c9e2e83b8926da62a87b5
dvosecky <daniel.j.vosecky@biola.edu>
2017-09-15 14:59:52 -0700

    working on setting up webpack and sass

--- directivePractice.js
+++ directivePractice.js
@@ -1,23 +1,10 @@
-var goalBarHtml = `
-	<div class="bar-background">
-		<div class="bar" style="width: calc(100% * {{current}}/{{total}})"></div>
-	</div>
-	<div class="row">
-		<h4>{{current}}</h4>
-		<h4>{{total}}</h4>
-	</div>
-	<div class="row">
-		<h5>{{currenttitle}}</h5>
-		<h5>{{totaltitle}}</h5>
-	</div>
-`;
-
 (function(angular) {
 angular.module('myDirectives', [])
 	.controller('Controller', ['$scope', function($scope) {
 		$scope.current = 42;
 		$scope.total = 100;
 	}])
+	.directive('goalBar', require('./goalBar/goalBar.js'))
 
   .directive('myHelloWorld', function() {
     return {
@@ -42,17 +29,5 @@ angular.module('myDirectives', [])
         });
       }
     };
-  }])
-	
-	.directive('goalBar', function() {
-		return {
-			scope: {
-				current: '@',
-				total: '@',
-				currenttitle: '@',
-				totaltitle: '@'
-			},
-			template: goalBarHtml,
-		}
-	})
+	}])
 })(window.angular);
